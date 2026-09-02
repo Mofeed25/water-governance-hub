7048979d1fe83725153afb5d0618bd81ed66142b
Mofeed25 <mueedabd92@gmail.com>
2026-09-02 23:06:13 +0300

    feat: add shared edge function CORS policy

---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,7 @@
+const allowedOrigin = Deno.env.get("MIZAN_APP_ORIGIN") ?? "*";
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": allowedOrigin,
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Vary": "Origin",
+};
